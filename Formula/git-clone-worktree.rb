130a8889140e1c004240dc313287d7d3104ae234
Franco Gallardo Grazio <10978027+fgallardograzio@users.noreply.github.com>
2026-05-18 01:51:24 -0300

    Update git-clone-worktree to use release artifact
    Closes #2.
    
    Signed-off-by: Franco Gallardo Grazio <10978027+fgallardograzio@users.noreply.github.com>

--- Formula/git-clone-worktree.rb
+++ Formula/git-clone-worktree.rb
@@ -2,8 +2,8 @@ class GitCloneWorktree < Formula
   desc "Clones a bare git repo and checks out the default branch as a worktree"
   homepage "https://github.com/fgallardograzio/git-clone-worktree"
 
-  url "https://github.com/fgallardograzio/git-clone-worktree/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "d392fab67c75ee8ba0aaf5e8a9418dfad8277d87dc5f0e155ed53ee35c12b60e"
+  url "https://github.com/fgallardograzio/git-clone-worktree/releases/download/v1.0.0/git-clone-worktree-v1.0.0.tar.gz"
+  sha256 "a91b36101c0dd4c234966857e9180a22e2ec49452d7342dfa90a361cc188b861"
   license "MIT"
 
   bottle do
@@ -14,14 +14,9 @@ class GitCloneWorktree < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "52628bfe4ce0fd895268af5facbc42dd2973fc8f5fee64688b34ba8b43ee3e8b"
   end
 
-  depends_on "pandoc" => :build
-
   def install
-    system "pandoc", "-f", "markdown-smart", "-s", "-t", "man",
-           "docs/git-clone-worktree.1.md", "-o", "git-clone-worktree.1"
-
-    man1.install "git-clone-worktree.1"
     bin.install "src/git-clone-worktree"
+    man1.install "man/git-clone-worktree.1"
   end
 
   test do
